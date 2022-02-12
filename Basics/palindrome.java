943c91311e6c1fb2ceb70c57c3d36e39d466a6d6
Adam Sadek <33073174+AdamSadek@users.noreply.github.com>
2022-02-12 18:14:46 +0000

    Update palindrome.java

--- Basics/palindrome.java
+++ Basics/palindrome.java
@@ -1,19 +1,11 @@
 class palindrome {
   public static void main(String[] args) {
-
-    String str = "Radar", reverseStr = "";
-    
-    int strLength = str.length();
-
-    for (int i = (strLength - 1); i >=0; --i) {
-      reverseStr = reverseStr + str.charAt(i);
+        isPalin("Radar");
     }
-
-    if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
-      System.out.println(str + " is a Palindrome String.");
+    public static String isPalin(String str){
+        str = str.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        sb.append(str);
+        return sb.reverse().toString().equals(str) ? "String is a palindrome." : "String is not a palindrome.";
     }
-    else {
-      System.out.println(str + " is not a Palindrome String.");
-    }
-  }
 }
