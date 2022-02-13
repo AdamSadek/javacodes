489104790d8cba0aa7dc50b9d1919d7d8c1175d3
Adam Sadek <33073174+AdamSadek@users.noreply.github.com>
2022-02-13 16:02:32 +0000

    Update mergeTwoLists.java

--- Basics/mergeTwoLists.java
+++ Basics/mergeTwoLists.java
@@ -2,19 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         List<Integer> firstList = new ArrayList<>();
-        List<Integer> secondList = new ArrayList<>();
-      
+        List<Integer> secondList = new ArrayList<>();   
+        
         firstList.add(1);
-        firstList.add(2);
-        firstList.add(3);
-        firstList.add(4);
-        firstList.add(5);
-        secondList.add(6);
-        secondList.add(7);
-        secondList.add(8);
-        secondList.add(9);
-        secondList.add(10);
-      
+        secondList.add(2);
+ 
         List<Integer> mergedLists = new ArrayList<>(firstList);
         mergedLists.addAll(secondList);
     }
